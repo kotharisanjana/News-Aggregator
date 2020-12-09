065a8af71b10dae4a448b088972f8abc814931da
kotharisanjana <60322201+kotharisanjana@users.noreply.github.com>
2020-12-09 11:01:30 +0530

    Update settings.py

--- contentaggregator/settings.py
+++ contentaggregator/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '978!(%*-#=^dacm1dv3*)guyj__f&ia$hz!9arz@8s6dnq=-9_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
